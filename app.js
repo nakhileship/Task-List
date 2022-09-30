6c86f44b2a515da414160e223906169bbcbbcd2f
nakhileship <www.nakhileshnakhu@gmail.com>
2022-10-01 00:04:16 +0530

    update

---
+++ app.js
@@ -0,0 +1,6 @@
+//Define UI Var
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-task');
+const filter =document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
