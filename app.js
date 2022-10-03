95ec952ec0600645345d6d676328f7ca4ba08236
nakhileship <www.nakhileshnakhu@gmail.com>
2022-10-03 23:59:25 +0530

    update

--- app.js
+++ app.js
@@ -2,21 +2,25 @@
 const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-task');
-const filter =document.querySelector('#filter');
+const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 
 //  Load all event listeners
 loadEventListners();
 
 // function all event listeners
-function loadEventListners(){
+function loadEventListners() {
     // Add task event
-    form.addEventListener('submit' , addTask);
+    form.addEventListener('submit', addTask);
+    // Remove task event
+    taskList.addEventListener('click', removeTask);
+    // clear task event
+    clearBtn.addEventListener('click', clearTask);
 }
 
 // Add Task
-function addTask(e){
-    if(taskInput.value == ''){
+function addTask(e) {
+    if (taskInput.value == '') {
         alert('Add a task');
     }
 
@@ -27,7 +31,7 @@ function addTask(e){
     // Create text node and append to li
     li.appendChild(document.createTextNode(taskInput.value));
     // create new link element
-    const link  = doacument.createElement('a');
+    const link = doacument.createElement('a');
     // add class
     link.className = 'delete-item secondary-content';
     // Add icon html
@@ -42,4 +46,23 @@ function addTask(e){
     taskInput.value = '';
 
     e.preventDefault();
-} 
+}
+
+// remove task
+function removeTask(e) {
+    if (e.target.parentElement.classList.contans('delere-item')) {
+        if (confirm('Are you sure?')) {
+            e.targetparentElement.paratElement.remove();
+        }
+    }
+
+}
+// clear task
+function clearTask() {
+    // taskList.innerHTML='';
+
+    // faster
+    while (taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+}
